4dc244d68aa861966edd60cb3598a4a67e75f573
thibault.scire <admin@joreci.net>
2025-02-18 17:19:21 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="sysbotCore",
+    name="sysbot",
     version="version",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
     author="Thibault SCIRE",
     author_email="thibault.scire@outlook.com",
-    description="Sysbot core library",
+    description="Sysbot library",
     long_description="""This library provides core functionalities for the SysBot project.""",
-    url="https://gitlab.com/home9344428/app/sysbot/sysbotCore.git",
+    url="https://gitlab.com/home9344428/app/sysbot/sysbot.git",
     license="MIT",
     license_files=['LICENSE'],
     python_requires=">=3.7",
