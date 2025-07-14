990d23b001f854278e25422d9afcca2bcd1e0c71
thibault.scire@outlook.com <thibault.scire@outlook.com>
2025-07-14 10:22:47 +0200

    Refactor: simplifier la gestion des connexions et des tunnels SSH avec TunnelingManager

--- sysbot/utils/__init__.py
+++ sysbot/utils/__init__.py
@@ -1,3 +1,4 @@
 from .security import Security
 from .engine.versionManager import VersionManager
 from .engine.ConnectorInterface import ConnectorInterface
+from .engine.tunneling import TunnelingManager
--- sysbot/utils/engine/__init__.py
+++ sysbot/utils/engine/__init__.py
@@ -1,2 +1,3 @@
 from .ConnectorInterface import ConnectorInterface
 from .versionManager import VersionManager
+from .tunneling import TunnelingManager
