3603dd10ffd0c6c55cd3b1cb4792fce6a4ab29b0
thibault.scire@outlook.com <thibault.scire@outlook.com>
2025-08-27 01:57:38 +0200

    finish systemd module

--- sysbot/modules/linux/systemd.py
+++ sysbot/modules/linux/systemd.py
@@ -10,3 +10,9 @@ class Systemd(BaseModule):
 
     def is_active(self, alias, name):
         return self.execute_command(alias, f"systemctl is-active {name}")
+    
+    def is_enabled(self, alias, name):
+        return self.execute_command(alias, f"systemctl is-enabled {name}")
+    
+    def is_failed(self, alias, name):
+        return self.execute_command(alias, f"systemctl is-failed {name}")
