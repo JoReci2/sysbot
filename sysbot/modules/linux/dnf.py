85eb04432850716e7e08560fb229ba371d036404
thibault.scire@outlook.com <thibault.scire@outlook.com>
2025-08-28 18:16:10 +0200

    update dnf module

--- sysbot/modules/linux/dnf.py
+++ sysbot/modules/linux/dnf.py
@@ -1,9 +1,17 @@
+from sysbot.utils.engine import BaseModule
 import json
 
-class Dnf(object):
+class Dnf(BaseModule):
 
-    def repolist(self, name):
-        return f"dnf repolist {name}"
-    
-    def list_installed(self, name):
-        return f"dnf list installed {name}"
+    def repolist(self, alias):
+        output = self.execute_command(alias, "dnf repolist --json")
+        return json.loads(output)
+
+    def get_package_version(self, alias, name):
+        return self.execute_command(alias, f"""rpm -q --queryformat="%{{VERSION}}" {name}""")
+
+    def get_package_release(self, alias, name):
+        return self.execute_command(alias, f"""rpm -q --queryformat="%{{RELEASE}}" {name}""")
+
+    def package_is_installed(self, alias, name):
+        return self.execute_command(alias, f"""rpm -q {name}""") == 0
