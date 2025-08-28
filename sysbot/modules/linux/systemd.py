0931368be008ce8252d90f3fe332946645cbef65
thibault.scire@outlook.com <thibault.scire@outlook.com>
2025-08-28 19:03:47 +0200

    update systemd module

--- sysbot/modules/linux/systemd.py
+++ sysbot/modules/linux/systemd.py
@@ -2,12 +2,6 @@ from sysbot.utils.engine import BaseModule
 
 class Systemd(BaseModule):
 
-    def start(self, alias, name):
-        return self.execute_command(alias, f"systemctl start {name}")
-
-    def stop(self, alias, name):
-        return self.execute_command(alias, f"systemctl stop {name}")
-
     def is_active(self, alias, name):
         return self.execute_command(alias, f"systemctl is-active {name}")
     
