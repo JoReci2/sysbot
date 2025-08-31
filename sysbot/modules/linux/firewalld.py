3c823c8160d3ac876cf5e077e9097c98f595a465
thibault.scire@outlook.com <thibault.scire@outlook.com>
2025-08-31 23:16:58 +0200

    Ajout de paramètres optionnels aux méthodes de récupération des zones dans le module Firewalld

--- sysbot/modules/linux/firewalld.py
+++ sysbot/modules/linux/firewalld.py
@@ -3,42 +3,42 @@ import json
 
 
 class Firewalld(ComponentBase):
-    def getZone(self, alias: str) -> dict:
-        output = self.execute_command(alias, "gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getZones")
+    def getZones(self, alias: str, **kwargs) -> dict:
+        output = self.execute_command(alias, "gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getZones", **kwargs)
         return json.loads(output)
 
-    def getActiveZones(self, alias: str) -> dict:
-        output = self.execute_command(alias, "gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getActiveZones")
+    def getActiveZones(self, alias: str, **kwargs) -> dict:
+        output = self.execute_command(alias, "gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getActiveZones", **kwargs)
         return json.loads(output)
 
-    def getDefaultZone(self, alias: str) -> str:
-        output = self.execute_command(alias, "gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.getDefaultZone")
-        return json.loads(output)
-    
-    def getForwardPorts(self, alias: str, zone: str) -> dict:
-        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getForwardPorts {zone}")
-        return json.loads(output)
-    
-    def getPorts(self, alias: str, zone: str) -> dict:
-        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getPorts {zone}")
+    def getDefaultZone(self, alias: str, **kwargs) -> str:
+        output = self.execute_command(alias, "gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.getDefaultZone", **kwargs)
         return json.loads(output)
 
-    def getInterface(self, alias: str, zone: str) -> dict:
-        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getInterface {zone}")
-        return json.loads(output)
-    
-    def getServices(self, alias: str, zone: str) -> dict:
-        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getServices {zone}")
+    def getForwardPorts(self, alias: str, zone: str, **kwargs) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getForwardPorts {zone}", **kwargs)
         return json.loads(output)
 
-    def getProtocols(self, alias: str, zone: str) -> dict:
-        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getProtocols {zone}")
+    def getPorts(self, alias: str, zone: str, **kwargs) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getPorts {zone}", **kwargs)
         return json.loads(output)
 
-    def getSourcePorts(self, alias: str, zone: str) -> dict:
-        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getSourcePorts {zone}")
+    def getInterface(self, alias: str, zone: str, **kwargs) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getInterface {zone}", **kwargs)
         return json.loads(output)
-    
-    def getSources(self, alias: str, zone: str) -> dict:
-        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getSources {zone}")
+
+    def getServices(self, alias: str, zone: str, **kwargs) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getServices {zone}", **kwargs)
+        return json.loads(output)
+
+    def getProtocols(self, alias: str, zone: str, **kwargs) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getProtocols {zone}", **kwargs)
+        return json.loads(output)
+
+    def getSourcePorts(self, alias: str, zone: str, **kwargs) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getSourcePorts {zone}", **kwargs)
+        return json.loads(output)
+
+    def getSources(self, alias: str, zone: str, **kwargs) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getSources {zone}", **kwargs)
         return json.loads(output)
