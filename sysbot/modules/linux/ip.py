fb6d8feb24ed250c9de264ea978d15415e443988
thibault.scire@outlook.com <thibault.scire@outlook.com>
2025-08-31 13:59:21 +0200

    Feature: ajout des méthodes link, resolve et ping à la classe Ip

--- sysbot/modules/linux/ip.py
+++ sysbot/modules/linux/ip.py
@@ -13,3 +13,13 @@ class Ip(ComponentBase):
     
     def speed(self, alias: str, interface: str) -> str:
         return self.execute_command(alias, f"cat /sys/class/net/{interface}/speed")
+
+    def link(self, alias: str, interface: str) -> dict:
+        output = self.execute_command(alias, f"ip --json link show {interface}")
+        return json.loads(output)
+
+    def resolve(self, alias: str, fqdn: str) -> dict:
+        return self.execute_command(alias, f"getent hosts {fqdn} | awk '{{print $1}}'")
+
+    def ping(self, alias: str, host: str) -> dict:
+        return self.execute_command(alias, f"ping -W 1 -c 1 {host} > /dev/null 2>&1 ; echo $?")
