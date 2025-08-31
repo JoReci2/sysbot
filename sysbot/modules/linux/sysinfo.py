97ba02f319d46081988b78acd663cc7a20f923d5
thibault.scire@outlook.com <thibault.scire@outlook.com>
2025-08-31 18:20:43 +0200

    Ajout de la méthode 'process' pour récupérer les informations des processus en cours d'exécution.

--- sysbot/modules/linux/sysinfo.py
+++ sysbot/modules/linux/sysinfo.py
@@ -68,3 +68,17 @@ class Sysinfo(ComponentBase):
                 key, value = line.split("=", 1)
                 env_vars[key] = value.strip()
         return env_vars
+
+    def process(self, alias: str) -> dict:
+        output = self.execute_command(alias, "ps -Aww -o pid,user,comm,pcpu,pmem")
+        processes = {}
+        for line in output.splitlines()[1:]:
+            if line:
+                pid, user, comm, pcpu, pmem = line.split(None, 4)
+                processes[pid] = {
+                    "user": user,
+                    "command": comm,
+                    "cpu": pcpu,
+                    "memory": pmem
+                }
+        return processes
