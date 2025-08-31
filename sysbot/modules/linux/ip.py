54272045c6553a39fb451fbc59a5522b17f5f31d
JoReci <157246302+JoReci2@users.noreply.github.com>
2025-08-31 14:17:04 +0200

    Features/module interface (#49)
    * Feature: ajout de la classe Ip pour gérer les adresses IP
    
    * Feature: ajout de la méthode route pour récupérer les informations de routage
    
    * Refactor: corriger les paramètres de la méthode addr et ajouter la méthode speed
    
    * Feature: ajout des méthodes link, resolve et ping à la classe Ip
    
    * Correction: modifier l'assertion de la méthode uptime pour vérifier les minutes au lieu des jours
    
    * Feature: ajout de tests pour les méthodes addr, route, speed, link, resolve et ping dans la classe linux.ip

---
+++ sysbot/modules/linux/ip.py
@@ -0,0 +1,25 @@
+from sysbot.utils.engine import ComponentBase
+import json
+
+class Ip(ComponentBase):
+
+    def addr(self, alias: str, interface: str) -> dict:
+        output = self.execute_command(alias, f"ip --json addr show {interface}")
+        return json.loads(output)
+
+    def route(self, alias: str) -> dict:
+        output = self.execute_command(alias, f"ip --json route")
+        return json.loads(output)
+    
+    def speed(self, alias: str, interface: str) -> str:
+        return self.execute_command(alias, f"cat /sys/class/net/{interface}/speed")
+
+    def link(self, alias: str, interface: str) -> dict:
+        output = self.execute_command(alias, f"ip --json link show {interface}")
+        return json.loads(output)
+
+    def resolve(self, alias: str, fqdn: str) -> dict:
+        return self.execute_command(alias, f"getent hosts {fqdn} | awk '{{print $1}}'")
+
+    def ping(self, alias: str, host: str) -> dict:
+        return self.execute_command(alias, f"ping -W 1 -c 1 {host} > /dev/null 2>&1 ; echo $?")
