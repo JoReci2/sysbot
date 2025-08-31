a8753713be9f9f2da0d1d7c81c30febf98ebb646
thibault.scire@outlook.com <thibault.scire@outlook.com>
2025-08-31 22:56:17 +0200

    Ajout de méthodes pour récupérer les ports, interfaces, services, protocoles, et sources dans le module Firewalld

--- sysbot/modules/linux/firewalld.py
+++ sysbot/modules/linux/firewalld.py
@@ -15,4 +15,30 @@ class Firewalld(ComponentBase):
         output = self.execute_command(alias, "gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.getDefaultZone")
         return json.loads(output)
     
+    def getForwardPorts(self, alias: str, zone: str) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getForwardPorts {zone}")
+        return json.loads(output)
     
+    def getPorts(self, alias: str, zone: str) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getPorts {zone}")
+        return json.loads(output)
+
+    def getInterface(self, alias: str, zone: str) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getInterface {zone}")
+        return json.loads(output)
+    
+    def getServices(self, alias: str, zone: str) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getServices {zone}")
+        return json.loads(output)
+
+    def getProtocols(self, alias: str, zone: str) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getProtocols {zone}")
+        return json.loads(output)
+
+    def getSourcePorts(self, alias: str, zone: str) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getSourcePorts {zone}")
+        return json.loads(output)
+    
+    def getSources(self, alias: str, zone: str) -> dict:
+        output = self.execute_command(alias, f"gdbus call --system   --dest org.fedoraproject.FirewallD1   --object-path /org/fedoraproject/FirewallD1   --method org.fedoraproject.FirewallD1.zone.getSources {zone}")
+        return json.loads(output)
