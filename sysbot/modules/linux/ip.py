0b7744c3d1b84a689b6609e9f11d8ac8e4735403
thibault.scire@outlook.com <thibault.scire@outlook.com>
2025-08-31 13:30:26 +0200

    Feature: ajout de la méthode route pour récupérer les informations de routage

--- sysbot/modules/linux/ip.py
+++ sysbot/modules/linux/ip.py
@@ -6,4 +6,7 @@ class Ip(ComponentBase):
     def addr(self, alias: str, name: str) -> dict:
         output = self.execute_command(alias, f"ip --json addr show {name}")
         return json.loads(output)
-    
+
+    def route(self, alias: str) -> dict:
+        output = self.execute_command(alias, f"ip --json route")
+        return json.loads(output)
