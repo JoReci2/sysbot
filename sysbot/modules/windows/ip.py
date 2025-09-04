cfd9c291391dc5510e5eb59f7f23de3e6b8e1278
JoReci <157246302+JoReci2@users.noreply.github.com>
2025-09-04 13:14:26 +0200

    Refactor network-related methods in ip.py

--- sysbot/modules/windows/ip.py
+++ sysbot/modules/windows/ip.py
@@ -3,12 +3,42 @@ import json
 
 
 class Sysinfo(ComponentBase):
-    def win32_networkadapter(self, alias: str, **kwargs) -> dict:
-        command = "Get-WmiObject -Class Win32_NetworkAdapter | Select-Object Name, DeviceID, MACAddress, AdapterType, Speed  | ConvertTo-Json"
+
+    def __get_cim_class__(namespace: str, classname: str, property: str) -> dict:
+        return f"Get-CimInstance -Namespace {namespace} -ClassName {classname} | Select-Object {property} | ConvertTo-Json"
+
+    def addr(self, alias: str, **kwargs) -> dict:
+        command = self.__get_cim_class__(
+            namespace="root\cimv2", 
+            classname="Win32_NetworkAdapterConfiguration", 
+            property="DHCPEnabled, IPAddress, IPSubnet, DefaultIPGateway, DNSServerSearchOrder, ServiceName, Index, MTU"
+        )
         output = self.execute_command(alias, command, **kwargs)
         return json.loads(output)
     
-    def win32_networkadapterconfiguration(self, alias: str, **kwargs) -> dict:
-        command = "Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Select-Object DHCPEnabled, IPAddress, IPSubnet, DefaultIPGateway, DNSServerSearchOrder, ServiceName, Index, MTU  | ConvertTo-Json"
+    def link(self, alias: str, **kwargs) -> dict:
+        command = self.__get_cim_class__(
+            namespace="root/StandardCimv2",
+            classname="MSFT_NetAdapter",
+            property="Name, Status, LinkSpeed, PhysicalMediaType, MacAddress"
+        )
         output = self.execute_command(alias, command, **kwargs)
         return json.loads(output)
+
+    def route(self, alias: str, **kwargs) -> dict:
+        command = self.__get_cim_class__(
+            namespace="root/StandardCimv2",
+            classname="MSFT_NetRoute",
+            property="InterfaceAlias, NextHop, State, DestinationPrefix"
+        )
+        output = self.execute_command(alias, command, **kwargs)
+        return json.loads(output)
+
+    def resolve(self, alias: str, fqdn: str, **kwargs) -> dict:
+        command = f"(Resolve-DnsName {fqdn}).IPAddress | ConvertTo-Json"
+        output = self.execute_command(alias, command, **kwargs)
+        return json.loads(output)
+
+    def ping(self, alias: str, host: str, **kwargs) -> dict:
+        command = f"Test-Connection -ComputerName {host} -Count 1 -Quiet"
+        return self.execute_command(alias, command, **kwargs)
