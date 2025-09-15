8c1a2dd110fbd48e508fb8789515dd95ea9393c9
thibault.scire@outlook.com <thibault.scire@outlook.com>
2025-09-15 22:40:03 +0200

    Fix: ajout de virgules manquantes dans les propriétés des commandes CIM

--- sysbot/modules/windows/ip.py
+++ sysbot/modules/windows/ip.py
@@ -4,21 +4,20 @@ import json
 
 
 class Ip(ComponentBase):
-
     def addr(self, alias: str, **kwargs) -> dict:
         command = Windows.get_cim_class(
-            namespace="root\cimv2", 
-            classname="Win32_NetworkAdapterConfiguration", 
-            property="DHCPEnabled, IPAddress, IPSubnet, DefaultIPGateway, DNSServerSearchOrder, ServiceName, Index, MTU"
+            namespace="root\cimv2",
+            classname="Win32_NetworkAdapterConfiguration",
+            property="DHCPEnabled, IPAddress, IPSubnet, DefaultIPGateway, DNSServerSearchOrder, ServiceName, Index, MTU",
         )
         output = self.execute_command(alias, command, **kwargs)
         return json.loads(output)
-    
+
     def link(self, alias: str, **kwargs) -> dict:
         command = Windows.get_cim_class(
             namespace="root/StandardCimv2",
             classname="MSFT_NetAdapter",
-            property="Name, Status, LinkSpeed, PhysicalMediaType, MacAddress"
+            property="Name, Status, LinkSpeed, PhysicalMediaType, MacAddress",
         )
         output = self.execute_command(alias, command, **kwargs)
         return json.loads(output)
@@ -27,7 +26,7 @@ class Ip(ComponentBase):
         command = Windows.get_cim_class(
             namespace="root/StandardCimv2",
             classname="MSFT_NetRoute",
-            property="InterfaceAlias, NextHop, State, DestinationPrefix"
+            property="InterfaceAlias, NextHop, State, DestinationPrefix",
         )
         output = self.execute_command(alias, command, **kwargs)
         return json.loads(output)
