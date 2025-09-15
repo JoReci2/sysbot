f5be4c74a5ab1ecf2d3c49ad2d88c1c5e3c45309
JoReci <157246302+JoReci2@users.noreply.github.com>
2025-09-15 22:31:52 +0200

    Feat/module ip for windows (#76)
    * Feat/module sysinfo: ajout de la classe Sysinfo pour récupérer les informations des adaptateurs réseau
    
    * Refactor network-related methods in ip.py
    
    * Add Windows class with get_cim_class method
    
    * Refactor command generation to use Windows helper
    
    * Rename Sysinfo class to Ip in ip.py

---
+++ sysbot/modules/windows/ip.py
@@ -0,0 +1,42 @@
+from sysbot.utils.engine import ComponentBase
+from sysbot.utils.helper import Windows
+import json
+
+
+class Ip(ComponentBase):
+
+    def addr(self, alias: str, **kwargs) -> dict:
+        command = Windows.get_cim_class(
+            namespace="root\cimv2", 
+            classname="Win32_NetworkAdapterConfiguration", 
+            property="DHCPEnabled, IPAddress, IPSubnet, DefaultIPGateway, DNSServerSearchOrder, ServiceName, Index, MTU"
+        )
+        output = self.execute_command(alias, command, **kwargs)
+        return json.loads(output)
+    
+    def link(self, alias: str, **kwargs) -> dict:
+        command = Windows.get_cim_class(
+            namespace="root/StandardCimv2",
+            classname="MSFT_NetAdapter",
+            property="Name, Status, LinkSpeed, PhysicalMediaType, MacAddress"
+        )
+        output = self.execute_command(alias, command, **kwargs)
+        return json.loads(output)
+
+    def route(self, alias: str, **kwargs) -> dict:
+        command = Windows.get_cim_class(
+            namespace="root/StandardCimv2",
+            classname="MSFT_NetRoute",
+            property="InterfaceAlias, NextHop, State, DestinationPrefix"
+        )
+        output = self.execute_command(alias, command, **kwargs)
+        return json.loads(output)
+
+    def resolve(self, alias: str, fqdn: str, **kwargs) -> dict:
+        command = f"(Resolve-DnsName {fqdn}).IPAddress | ConvertTo-Json"
+        output = self.execute_command(alias, command, **kwargs)
+        return json.loads(output)
+
+    def ping(self, alias: str, host: str, **kwargs) -> dict:
+        command = f"Test-Connection -ComputerName {host} -Count 1 -Quiet"
+        return self.execute_command(alias, command, **kwargs)
