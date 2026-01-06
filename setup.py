8d823e773157252f30b8e61f74dad48e06249786
thibault.scire@outlook.com <thibault.scire@outlook.com>
2026-01-06 18:20:27 +0100

    Mettre à jour la version du package à 0.2.0 dans setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sysbot",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
